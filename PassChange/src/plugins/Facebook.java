07bbd5d8f6a6c6d6427009eac75573427b0d30c9
klg71 <klg71@web.de>
2014-06-12 10:04:53 +0200

    Adding Facebook class

---
+++ PassChange/src/plugins/Facebook.java
@@ -0,0 +1,24 @@
+package plugins;
+
+import core.Website;
+
+public class Facebook extends Website{
+
+	public Facebook(String username, String pass) {
+		super(username, pass);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void authenticate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changePassword(String newPass) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
