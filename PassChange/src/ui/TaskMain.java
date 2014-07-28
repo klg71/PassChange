2925573c737fe50d35bf4480262a8b244b303f71
klg71 <klg71@web.de>
2014-07-28 10:24:33 +0200

    autgreyin passwords

--- PassChange/src/ui/TaskMain.java
+++ PassChange/src/ui/TaskMain.java
@@ -1,7 +1,10 @@
 package ui;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Scanner;
 
 import core.RequestType;
 import core.WebClient;
@@ -11,11 +14,21 @@ public class TaskMain {
 
 	public static void main(String[] args) {
 		WebClient webClient=new WebClient();
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File("passwords"));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String password=scanner.nextLine();
+		
 		webClient.sendRequest("https://www.facebook.com/home.php", RequestType.GET,"","","home1");
+		System.out.println(password);
 		//System.out.println(webClient.getCookies());
 		try {
 			webClient.sendRequest("https://login.facebook.com/login.php", RequestType.POST,"email=" + URLEncoder.encode( "klg71@web.de", "UTF-8" ) + "&" +
-				          "pass=" + URLEncoder.encode( "***REMOVED***", "UTF-8" ),"","login");
+				          "pass=" + URLEncoder.encode( password, "UTF-8" ),"","login");
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
